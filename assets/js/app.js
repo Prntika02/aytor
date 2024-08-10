e3cd27fa548ccc2c25905cdc70c351ef2a2042e2
prantikamutsuddi <prantika375@gmail.com>
2024-08-10 20:19:15 +0600

    day5

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,6 +38,120 @@ $(function(){
 
 
     })
+    $('.main_slider').slick({
+      arrows:false,
+      dots:true,
+      dotsClass:'bannerDots container',
+      autoplay: true,
+      autoplaySpeed: 1800,
+  });
 
 })
 
+
+
+
+//ARRIVALS 
+
+$('.parent_arrivals').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    arrows:true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    prevArrow: '<span class="left"><i class="fa-solid fa-chevron-left"></i></span>',
+    nextArrow:'<span class="right"><i class="fa-solid fa-chevron-right"></i></span>', 
+  
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+
+//* ARRIVALS JS END
+
+//tooltip 
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//tooltip ends
+
+// today venobox
+
+new VenoBox({
+  selector: '.explore',
+});
+// today venobox
+
+
+        
+      // latest *****************************
+      $('.latest').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,  
+        autoplaySpeed: 2000,
+        arrows:false,
+        dots:true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+         
+        ]
+      });
+     
+  
+
+  // latest ends
